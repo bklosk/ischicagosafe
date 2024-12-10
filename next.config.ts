022a601fbdd8919e7935e27e3aa8ab5dc1dae4e0
Ben Klosky <benklosky@uchicago.edu>
2024-12-09 22:14:39 -0600

    fixing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  output: "standalone",
 };
-
-export default nextConfig;
